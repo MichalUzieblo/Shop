d74eee613659b442c49c3d1396532dc0abf93c2f
MichalUzieblo <michal_uzieblo@op.pl>
2017-07-04 22:23:37 +0200

    cange title from string on php variable

--- src/html/htmlHeader.php
+++ src/html/htmlHeader.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shop</title>
+    <title><?php echo $title ?></title>
     <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <!-- JavaScript plugins (requires jQuery) -->
     <script src="https://code.jquery.com/jquery.js"></script>
