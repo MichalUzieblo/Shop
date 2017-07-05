3cc2183a62d6ddbb5d751f011ad6402deab30ba3
MichalUzieblo <michal_uzieblo@op.pl>
2017-07-05 22:37:49 +0200

    added queries to create two tables: Orders and Products_Orders

--- src/db/create_database.php
+++ src/db/create_database.php
@@ -35,6 +35,24 @@ $shopArraysSQL = array(
                         password varchar(60) NOT NULL,
                         PRIMARY KEY(id))
      ENGINE=InnoDB, CHARACTER SET=utf8"
+,   
+    "create table Orders(
+                        id int AUTO_INCREMENT NOT NULL,
+                        user_id int NOT NULL,
+                        status varchar(32) NOT NULL,
+                        PRIMARY KEY(id),
+                        FOREIGN KEY(user_id) REFERENCES Users(id) ON DELETE CASCADE)
+     ENGINE=InnoDB, CHARACTER SET=utf8"
+,    
+    "create table Products_Orders(
+                        id int AUTO_INCREMENT NOT NULL,
+                        product_id int NOT NULL,
+                        order_id int NOT NULL,
+                        PRIMARY KEY(id),
+                        FOREIGN KEY(order_id) REFERENCES Orders(id) ON DELETE CASCADE,
+                        FOREIGN KEY(product_id) REFERENCES Products(id) ON DELETE CASCADE)
+     ENGINE=InnoDB, CHARACTER SET=utf8"
+    
     );
 
 foreach($shopArraysSQL as $query){
